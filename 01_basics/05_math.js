c2ca9dfaf08cf17bf527f7402e6d033715b4a648
Shiva12k4 <120860468+Shiva12k4@users.noreply.github.com>
2024-09-18 12:56:39 +0000

    js

---
+++ 01_basics/05_math.js
@@ -0,0 +1,12 @@
+let number=400;
+console.log(number);
+let newNumber=new Number(400);
+console.log(newNumber);
+console.log(newNumber.toFixed(1));
+
+let count= 1432.1;
+console.log(count.toPrecision(5));//return string
+
+let num = 100000;
+console.log(num)
+
